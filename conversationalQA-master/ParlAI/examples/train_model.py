7b3d45f1534c972ee994529937de616f906f5eda
jochemsoons <jochemsoons98@hotmail.com>
2021-11-13 22:04:16 +0100

    ParlAI path fix

--- conversationalQA-master/ParlAI/examples/train_model.py
+++ conversationalQA-master/ParlAI/examples/train_model.py
@@ -9,8 +9,9 @@ Train a model using parlai's standard training loop.
 For documentation, see parlai.scripts.train_model.
 """
 import sys
-# sys.path.append('/home/jochemsoons/AI_MSC_UBUNTU/IR2/Repository/conversationalQA-master/ParlAI')
-sys.path.append('/home/lcur0071/Reproduction_repo/conversationalQA-master/ParlAI')
+# IMPORTANT: Append your local path to the ParlAi folder
+sys.path.append('/YOUR_PATH_TO/ParlAI')
+
 from parlai.scripts.train_model import TrainModel
 
 if __name__ == '__main__':
